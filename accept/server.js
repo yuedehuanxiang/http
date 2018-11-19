02bb1f1a64191c27d12e9ec42b68a12ac2ae8d0e
moonnoillusion <2506887658@qq.com>
2018-11-19 20:50:27 +0800

    content-type 数据协商

--- accept/server.js
+++ accept/server.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 const zlib = require('zlib')
 
 http.createServer(function (request, response) {
-    if (request.url === '/') {
-        const html = fs.readFileSync('test.html');
-        response.writeHead(200, {
-            'Content-Type': 'text/html', //这里一定要指定内容类型
-            'Content-Encoding': 'gzip'
-        })
-        response.end(zlib.gzipSync(html));
 
-    }
+    const html = fs.readFileSync('test.html');
+    response.writeHead(200, {
+        'Content-Type': 'text/html', //这里一定要指定内容类型
+        'Content-Encoding': 'gzip'
+    })
+    response.end(zlib.gzipSync(html));
+
+
 
 }).listen(8888);
